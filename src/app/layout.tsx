15ab38e82dbe6f4c6ba40fc0cce8ebadf0a3ccfb
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2024-10-18 17:06:04 -0300

    adding eslint prettier plugin, tailwind prettier plugin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,28 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 
-import "./globals.css";
+import './globals.css';
 
 const roboto = Roboto({
-  variable: "--font-roboto",
-  weight: ["100", "300","400", "500", "700", "900"],
-  subsets: ["latin"],
-  display: "swap",
+  variable: '--font-roboto',
+  weight: ['100', '300', '400', '500', '700', '900'],
+  subsets: ['latin'],
+  display: 'swap'
 });
 
 export const metadata: Metadata = {
-  title: "Next Template",
-  description: "Nexte Template",
+  title: 'Next Template',
+  description: 'Nexte Template'
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${roboto.className} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${roboto.className} antialiased`}>{children}</body>
     </html>
   );
 }
