3aa17b04e4ce0e40ff70afa4ce46f6607f22da79
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2024-10-18 17:42:43 -0300

    feat: 🎸 create Text polimorphic component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import { Button } from '@/components/ui/button';
+import { Text } from '@/components/ui/text';
 
 export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-center">
       <h1 className="mb-4 text-6xl font-light">Next Template</h1>
       <Button className="min-w-52">Click Me</Button>
+      <Text as="h1" size="lg" className="mb-4">
+        H1
+      </Text>
+      <Text as="h2" size="sm" className="mb-4">
+        H2
+      </Text>
+      <Text>Paragraph</Text>
     </main>
   );
 }
